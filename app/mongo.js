3ac48ec16f8c2075dd8b51f1d150898c5dbfc746
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 15:46:39 -0700

    Refactor models

--- app/mongo.js
+++ app/mongo.js
@@ -1,63 +1,13 @@
 var path = require('path');
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+
 mongoose.connect('mongodb://localhost:27017');
 
 var db = mongoose.connection;
 
-var userSchema = new Schema({
-  _id: Number,
-  username: String,
-  password: String,
-  date: { type: Date, default: Date.now }
+db.on('error', console.error.bind(console, 'cnnection error:'));
+db.once('open', function() {
+  console.log('Mongodb connection is open!');
 });
-
-var linkSchema = new Schema({
-  _creator: { type: Number, ref: 'User' },
-  url: String,
-  baseUrl: String,
-  code: String,
-  title: String,
-  visits: Number,
-  date: { type: Date, default: Date.now }
-});
-
-var User = mongoose.model('User', userSchema);
-var Link = mongoose.model('Link', linkSchema);
 
 module.exports = db;
-
-
-
-// var db = require('bookshelf')(knex);
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('baseUrl', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-
